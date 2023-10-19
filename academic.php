c0d98d31a50e86e63f572d021fe5db29e23e895c
AreenF03 <148501373+AreenF03@users.noreply.github.com>
2023-10-20 00:50:04 +0300

    Add files via upload

---
+++ academic.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        body{
+            background-color: #00ffff;
+            
+        }
+    </style>
+</head>
+<body>
+    <h1 style="text-align:center; font-size:80px;">عهد االتكنلوجيا</h1>
+    
+</body>
+</html>
